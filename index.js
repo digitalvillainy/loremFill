d455e987e8b2bd51c9d22a170c6cbbc6155eec0d
Roberto Rivera <bobby@roberto-rivera.com>
2019-09-23 23:33:13 -0400

    Updated files to only include the js files.

--- index.js
+++
@@ -1,2 +0,0 @@
-let App = require('./loremFill.js');
-App.beingFill();
--- loremFill.js
+++ loremFill.js
@@ -27,7 +27,7 @@ let loremText = {
         "paragraph": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
     };
 const loremFill = {
-    beingFill: function () {
+    beginFill: function () {
         loremTargets.forEach((element) => {
             if (element.innerHTML.trim() === undefined && element.innerHTML.trim() !== '') {
                 outputText = this.outputLorem(element.innerText);
