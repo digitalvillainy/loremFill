5c0c3d4126cbf181e159c42989ccdb59d2f2c64c
Roberto Rivera <bobby@roberto-rivera.com>
2019-09-26 01:54:07 -0400

    Updated filler text to be randomized

--- loremFill.js
+++ loremFill.js
@@ -1,1 +1,165 @@
-let tag,tagAttributes,childNode,childNodeAttributes,outputText,listArr,loremTargets=document.querySelectorAll("[lorem-fill]"),loremText={fill:"Lorem",sent:"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",list:["Lorem"," ipsum"," dolor"," sit"," amet"," consectetur"," adipiscing"," elit"," sed"," do"," eiusmod"," tempor"," incididunt"," ut"," labore"," et"," dolore"," magna"," aliqua"],paragraph:"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."};const loremFill={beginFill:function(){loremTargets.forEach(e=>{if(void 0===e.innerHTML.trim()&&""!==e.innerHTML.trim())outputText=this.outputLorem(e.innerText),(tag=document.createElement(e.nodeName.toLowerCase())).innerHTML=outputText,tagAttributes=e.attributes,this.cloneAttributes(tagAttributes),e.attributes["lorem-fill"]&&e.hasChildNodes()&&(e.innerText=outputText,this.cloneElements(e,Number(e.attributes["lorem-fill"].value)-1)),this.cloneElements(e,Number(e.attributes["lorem-fill"].value));else if(e.hasChildNodes()&&null!==e.lastElementChild){if(outputText=this.outputLorem(e.innerText),tag=document.createElement(e.nodeName.toLowerCase()),e.hasChildNodes()){e.childNodes.forEach(e=>{outputText=this.outputLorem(e.innerText),""!==e.innerText&&(listArr=Array.from(outputText),e.innerText=this.randomList(),childNode=document.createElement(e.nodeName.toLowerCase()),childNodeAttributes=e.attributes,this.cloneAttributes(childNodeAttributes,!0),childNode.innerHTML=this.randomList(),tag.append(childNode))})}this.cloneElements(e,Number(e.attributes["lorem-fill"].value)-1)}else{null!==new RegExp("{{.*").exec(e.innerText)&&(outputText=this.outputLorem(e.innerText),e.innerText=outputText,(tag=document.createElement(e.nodeName.toLowerCase())).innerHTML=outputText,tagAttributes=e.attributes,this.cloneAttributes(tagAttributes),this.cloneElements(e,Number(e.attributes["lorem-fill"].value)-1))}})},randomList:()=>listArr[Math.floor(Math.random()*listArr.length)],outputLorem:function(e){return new RegExp("{{.*").exec(e)?"{{lorem-sent}}"===e?loremText.sent:"{{lorem-pg}}"===e?loremText.paragraph:"{{lorem-list}}"===e?loremText.list:"{{lorem}}"===e?loremText.fill:void 0:e},cloneElements:function(e,t){let i,o=e,l=document.createDocumentFragment();console.log(),l.append(tag);let r=t;for(let e=0;e<r;e++)null!==tag.lastElementChild&&(childNode.innerHTML=this.randomList(),tag.append(childNode)),i=l.cloneNode(!0),o.after(i)},cloneAttributes:function(e,t=!1){for(let i=0;i<e.length;i++)"lorem-fill"!==e[i].nodeName&&(t?childNode.setAttribute(e[i].nodeName,e[i].nodeValue):tag.setAttribute(e[i].nodeName,e[i].nodeValue))}};loremFill.beginFill(),module.exports=loremFill;
+let tag, tagAttributes, childNode, childNodeAttributes, outputText, listArr, finalParagraph;
+let finalArr = [];
+let createParagraph = [];
+let loremText = {
+    "fill": [
+        "lorem",
+        "ipsum",
+        "dolor",
+        "sit",
+        "amet",
+        "consectetur",
+        "adipiscing",
+        "elit",
+        "sed",
+        "do",
+        "eiusmod",
+        "tempor",
+        "incididunt",
+        "ut",
+        "labore",
+        "et",
+        "dolore",
+        "magna",
+        "aliqua",
+        "enim",
+        "ad",
+        "minim",
+        "veniam",
+        "quis",
+        "nostrud",
+        "exercitation",
+        "ullamco",
+        "laboris",
+        "nisi",
+        "ut",
+        "aliquip",
+        "ex",
+        "ea",
+        "commodo",
+        "consequat",
+        "duis",
+        "aute",
+        "irure",
+        "in",
+        "reprehenderit",
+        "voluptate",
+        "velit",
+        "esse",
+        "cillum",
+        "dolore",
+        "eu",
+        "fugiat",
+        "nulla",
+        "pariatur",
+        "excepteur",
+        "sint",
+        "occaecat",
+        "cupidatat",
+        "non",
+        "proident",
+        "sunt",
+        "culpa",
+        "qui",
+        "officia",
+        "deserunt",
+        "mollit",
+        "anim",
+        "id",
+        "est",
+        "laborum"
+    ]
+};
+
+let loremTargets = document.querySelectorAll('[lorem-pg], [lorem-sent], [lorem]');
+console.log(loremTargets);
+listArr = loremText.fill;
+const loremFill = {
+    beginFill: function () {
+        loremTargets.forEach((element) => {
+            if (element.childNodes.length === 0) {
+                outputText = this.outputLorem(element.attributes[0].nodeName);
+                console.log(outputText);
+            } else if (element.childNodes.length !== 0 && element.lastElementChild !== null) {
+                console.log(element);
+            }
+
+        });
+    },
+
+    randomList: function (num, repeat = false) {
+        if (!repeat) {
+            return listArr[Math.floor(Math.random() * listArr.length)];
+        } else {
+            for (let i = 0; i < num; i++) {
+                finalArr[i] = listArr[Math.floor(Math.random() * listArr.length)];
+            }
+            return finalArr;
+        }
+    },
+
+    createSentence: function (num, repeat = false) {
+        if (num === 0 && !repeat) {
+            this.randomList(8, true);
+            let sentence = finalArr.join();
+            let finalSentence = sentence.replace(/,/g, ' ');
+            finalSentence = finalSentence.charAt(0).toUpperCase() + finalSentence.slice(1);
+            return finalSentence + '.';
+        } else {
+            for (let i = 0; i < num; i++) {
+                this.randomList(8, true);
+                let sentence = finalArr.join();
+                let finalSentence = sentence.replace(/,/g, ' ');
+                finalSentence = finalSentence.charAt(0).toUpperCase() + finalSentence.slice(1);
+                createParagraph[i] = finalSentence + '.';
+                let pg = createParagraph.join();
+                finalParagraph = pg.replace(/,/g, ' ');
+                finalParagraph.toString();
+            }
+            return finalParagraph;
+        }
+
+    },
+
+    outputLorem: function (cmd) {
+        if (cmd.includes('lorem-sent')) {
+            return this.createSentence(0);
+        } else if (cmd.includes('lorem-pg')) {
+            return this.createSentence(10, true);
+        } else if (cmd.includes('lorem')) {
+            return this.randomList(0);
+        }
+    },
+
+    cloneElements: function (el, repeatValue) {
+        let clonedParent = el;
+        let frag = document.createDocumentFragment();
+        frag.append(tag);
+        let handle;
+        let loremValue = repeatValue;
+        for (let i = 0; i < loremValue; i++) {
+            if (tag.lastElementChild !== null) {
+                childNode.innerHTML = this.randomList();
+                tag.append(childNode);
+            }
+            handle = frag.cloneNode(true);
+            clonedParent.after(handle);
+        }
+    },
+
+    cloneAttributes: function (attr, innerChild = false) {
+        for (let i = 0; i < attr.length; i++) {
+            if (attr[i].nodeName === 'lorem-fill') {
+                continue;
+            }
+            if (!innerChild) {
+                tag.setAttribute(attr[i].nodeName, attr[i].nodeValue);
+            } else {
+                childNode.setAttribute(attr[i].nodeName, attr[i].nodeValue);
+            }
+        }
+    },
+};
+loremFill.beginFill();
+
+
